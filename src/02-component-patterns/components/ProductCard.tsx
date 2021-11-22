5fe84e5824fd45a11d5f87ce97456945ff618a8a
Orlin Alvarado <oalvaradomorales@gmail.com>
2021-11-22 08:16:29 -0600

    Component Extend Styles - Listo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -2,13 +2,20 @@ import { createContext } from 'react';
 import styles from '../styles/styles.module.css';
 
 import { useProduct } from '../hooks/useProduct';
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
+import { ProductContextProps, Product } from '../interfaces/interfaces';
 
 export const ProductContext = createContext({} as ProductContextProps)
 
 const { Provider } = ProductContext
 
-export const ProductCard = ({ product, children }: ProductCardProps) => {
+export interface Props {
+    product: Product;    
+    children?: React.ReactElement | React.ReactElement[];
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+export const ProductCard = ({ product, children, className, style }: Props) => {
     
     const { counter, increaseBy } = useProduct();
     
@@ -18,7 +25,10 @@ export const ProductCard = ({ product, children }: ProductCardProps) => {
             increaseBy,
             product
         }}>
-            <div className={ styles.productCard }>
+            <div 
+                className={ `${ styles.productCard } ${ className }` } 
+                style={ style } 
+            >
             {
                 children
             }
--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -3,7 +3,13 @@ import noImage from '../assets/no-image.jpg'
 import { ProductContext } from './ProductCard';
 import styles from '../styles/styles.module.css';
 
-export const ProductImage = ({ img= ''}) => {
+export interface Props {
+    img?: string,
+    className?: string,
+    style?: React.CSSProperties;
+}
+
+export const ProductImage = ({ img, className, style  }: Props) => {
     const { product } = useContext(ProductContext)
     let imgToShow: string;
     
@@ -15,6 +21,11 @@ export const ProductImage = ({ img= ''}) => {
         imgToShow = noImage;
     }
     return (
-        <img className={ styles.productImg } src={ imgToShow } alt="Coffee Mug" />
+        <img 
+            className={ `${ styles.productImg } ${ className }` } 
+            src={ imgToShow } 
+            style={ style }
+            alt="Coffee Mug" 
+        />
     )
 }
